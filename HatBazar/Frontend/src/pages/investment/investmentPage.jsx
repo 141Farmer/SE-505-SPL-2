f386e0e6136f1d3a27b73947fb4eec52028ef842
kamrul <bsse1418@iit.du.ac.bd>
2025-01-16 13:17:09 +0600

    investment tab attempt

--- HatBazar/Frontend/src/pages/investment/investmentPage.jsx
+++ HatBazar/Frontend/src/pages/investment/investmentPage.jsx
@@ -52,26 +52,27 @@ function InvestmentPage() {
 
   return (
     
-    <div className="max-w-4xl mx-auto p-4">
-      <Navbar/>
-      <Tabs defaultValue="browse" className="w-full">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="browse">Browse Offers</TabsTrigger>
-          <TabsTrigger value="create">Create Offer</TabsTrigger>
-        </TabsList>
+    <div className="pt-16 max-w-4xl mx-auto p-4">
+      <Navbar />
+        <Tabs defaultValue="browse" className="w-full">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="browse">Browse Offers</TabsTrigger>
+            <TabsTrigger value="create">Create Offer</TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="browse">
-          <OffersGrid 
-            offers={[...offers, ...activeOffers]} 
-            onNegotiate={handleNegotiate}
-          />
-        </TabsContent>
+            <TabsContent value="browse">
+              <OffersGrid 
+               offers={[...offers, ...activeOffers]} 
+                onNegotiate={handleNegotiate}
+              />
+            </TabsContent>
 
-        <TabsContent value="create">
-          <CreateOffer onCreateOffer={handleCreateOffer} />
-        </TabsContent>
-      </Tabs>
+            <TabsContent value="create">
+              <CreateOffer onCreateOffer={handleCreateOffer} />
+            </TabsContent>
+        </Tabs>
     </div>
+
   );
 }
 
